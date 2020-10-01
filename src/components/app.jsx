c73af638ffae836d4a9f35faa3284b61d004cac8
xqt19 <tan.xiu.qi.11@gmail.com>
2020-10-01 14:31:44 +0800

    new day

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
+import CityList from '../containers/city_list'
+import City from '../containers/city'
 
-const App = () => {
-  return (
-    <div className="app">
-      <p>React + Redux starter</p>
-      <br />hello world
-    </div>
-  );
+class App extends Component{
+  render(){
+      return (
+        <div className="app" style ={{height:"100vh"}}>
+          <CityList />
+          <City />
+        </div>
+      );
+  }
 };
+// const App = () => {
+//   return (
+//     <div className="app">
+//       <p>React + Redux starter</p>
+//       <CityList />
+//       <City />
+//     </div>
+//   );
+// };
 
 export default App;
---
+++ src/containers/city.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class City extends Component{
+    render(){
+        return(
+            <div className="col-sm-5">
+                TODO
+            </div>
+        )
+    }
+}
+
+export default City
